f684c192f86ab90e2196b3b3fabfcd1bf016ebc6
Pyrofab <pyrofab@ladysnake.org>
2024-05-16 22:14:14 +0200

    Add model customization to fake player illustration

--- src/main/java/org/ladysnake/blabber/impl/common/illustrations/DialogueIllustrationFakePlayer.java
+++ src/main/java/org/ladysnake/blabber/impl/common/illustrations/DialogueIllustrationFakePlayer.java
@@ -19,20 +19,31 @@ package org.ladysnake.blabber.impl.common.illustrations;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Arm;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.ladysnake.blabber.api.DialogueIllustrationType;
+import org.ladysnake.blabber.impl.mixin.PlayerEntityAccessor;
+import org.ladysnake.blabber.impl.mixin.client.AbstractClientPlayerEntityAccessor;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class DialogueIllustrationFakePlayer extends DialogueIllustrationEntity<DialogueIllustrationFakePlayer.Spec> {
@@ -46,6 +57,7 @@ public class DialogueIllustrationFakePlayer extends DialogueIllustrationEntity<D
                     buf.readFloat(),
                     buf.readOptional(PacketByteBuf::readInt),
                     buf.readOptional(PacketByteBuf::readInt),
+                    buf.readOptional(PlayerModelOptions::new),
                     buf.readOptional(PacketByteBuf::readNbt)
             )),
             (buf, i) -> {
@@ -58,6 +70,7 @@ public class DialogueIllustrationFakePlayer extends DialogueIllustrationEntity<D
                 buf.writeFloat(i.spec().yOff());
                 buf.writeOptional(i.spec().stareAtX(), PacketByteBuf::writeInt);
                 buf.writeOptional(i.spec().stareAtY(), PacketByteBuf::writeInt);
+                buf.writeOptional(i.spec().modelOptions(), (b, opts) -> opts.writeToBuffer(b));
                 buf.writeOptional(i.spec().data(), PacketByteBuf::writeNbt);
             }
     );
@@ -71,21 +84,83 @@ public class DialogueIllustrationFakePlayer extends DialogueIllustrationEntity<D
         return TYPE;
     }
 
+    @SuppressWarnings("UnreachableCode")
     @Environment(EnvType.CLIENT)
     @Override
     protected @Nullable LivingEntity getRenderedEntity(World world) {
-        OtherClientPlayerEntity fakePlayer = new OtherClientPlayerEntity(
-                (ClientWorld) world,
-                this.spec().profile()
-        );
+        GameProfile profile = this.spec().profile();
+        OtherClientPlayerEntity fakePlayer = new OtherClientPlayerEntity((ClientWorld) world, profile);
         this.spec().data().ifPresent(fakePlayer::readNbt);
+        ((AbstractClientPlayerEntityAccessor) fakePlayer).setPlayerListEntry(new PlayerListEntry(profile, false));
         fakePlayer.prevBodyYaw = fakePlayer.bodyYaw = 0.0f;
         fakePlayer.prevHeadYaw = fakePlayer.headYaw = 0.0f;
+        PlayerModelOptions playerModelOptions = this.spec().modelOptionsOrDefault();
+        fakePlayer.getDataTracker().set(PlayerEntityAccessor.getPlayerModelParts(), playerModelOptions.packVisibleParts());
+        fakePlayer.setMainArm(playerModelOptions.mainHand());
         return fakePlayer;
     }
 
-    public record Spec(GameProfile profile, int x1, int y1, int x2, int y2, int size, float yOff, Optional<Integer> stareAtX,
-                       Optional<Integer> stareAtY, Optional<NbtCompound> data) implements DialogueIllustrationEntity.Spec {
+    public record PlayerModelOptions(Arm mainHand, EnumSet<PlayerModelPart> visibleParts) {
+        public static final PlayerModelOptions DEFAULT = new PlayerModelOptions(Arm.RIGHT, EnumSet.allOf(PlayerModelPart.class));
+        private static final Map<String, PlayerModelPart> partsByName;
+
+        static {
+            partsByName = new HashMap<>();
+            for (PlayerModelPart part : PlayerModelPart.values()) {
+                partsByName.put(part.getName(), part);
+            }
+        }
+
+        private static DataResult<PlayerModelPart> partFromString(String key) {
+            PlayerModelPart part = partsByName.get(key);
+            if (part != null) return DataResult.success(part);
+            return DataResult.error(() -> "Not a valid player model part " + key + " (should be one of " + partsByName.keySet() + ")");
+        }
+
+        public static final Codec<EnumSet<PlayerModelPart>> PLAYER_MODEL_PARTS_CODEC = Codec.list(Codec.STRING.comapFlatMap(
+                PlayerModelOptions::partFromString,
+                PlayerModelPart::getName
+        )).xmap(l -> {
+            EnumSet<PlayerModelPart> ret = EnumSet.noneOf(PlayerModelPart.class);
+            ret.addAll(l);
+            return ret;
+        }, List::copyOf);
+        public static final EnumSet<PlayerModelPart> NO_VISIBLE_PARTS = EnumSet.allOf(PlayerModelPart.class);
+
+        public static final Codec<PlayerModelOptions> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                Codecs.createStrictOptionalFieldCodec(StringIdentifiable.createBasicCodec(Arm::values), "main_hand", Arm.RIGHT).forGetter(PlayerModelOptions::mainHand),
+                Codecs.createStrictOptionalFieldCodec(PLAYER_MODEL_PARTS_CODEC, "visible_parts", NO_VISIBLE_PARTS).forGetter(PlayerModelOptions::visibleParts)
+        ).apply(instance, PlayerModelOptions::new));
+
+        public PlayerModelOptions(PacketByteBuf buf) {
+            this(buf.readEnumConstant(Arm.class), buf.readEnumSet(PlayerModelPart.class));
+        }
+
+        public void writeToBuffer(PacketByteBuf buf) {
+            buf.writeEnumConstant(this.mainHand);
+            buf.writeEnumSet(this.visibleParts, PlayerModelPart.class);
+        }
+
+        public byte packVisibleParts() {
+            byte packed = 0;
+            for (PlayerModelPart playerModelPart : this.visibleParts()) {
+                packed |= (byte) playerModelPart.getBitFlag();
+            }
+            return packed;
+        }
+    }
+
+    public record Spec(GameProfile profile,
+                       int x1,
+                       int y1,
+                       int x2,
+                       int y2,
+                       int size,
+                       float yOff,
+                       Optional<Integer> stareAtX,
+                       Optional<Integer> stareAtY,
+                       Optional<PlayerModelOptions> modelOptions,
+                       Optional<NbtCompound> data) implements DialogueIllustrationEntity.Spec {
         private static final MapCodec<Spec> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
                 Codecs.GAME_PROFILE_WITH_PROPERTIES.fieldOf("profile").forGetter(Spec::profile),
                 Codec.INT.fieldOf("x1").forGetter(Spec::x1),
@@ -96,7 +171,12 @@ public class DialogueIllustrationFakePlayer extends DialogueIllustrationEntity<D
                 Codecs.createStrictOptionalFieldCodec(Codec.FLOAT, "y_offset", 0.0f).forGetter(Spec::yOff),
                 Codecs.createStrictOptionalFieldCodec(Codec.INT, "stare_at_x").forGetter(Spec::stareAtX),
                 Codecs.createStrictOptionalFieldCodec(Codec.INT, "stare_at_y").forGetter(Spec::stareAtY),
+                Codecs.createStrictOptionalFieldCodec(PlayerModelOptions.CODEC, "model_customization").forGetter(Spec::modelOptions),
                 Codecs.createStrictOptionalFieldCodec(NbtCompound.CODEC, "data").forGetter(Spec::data)
         ).apply(instance, Spec::new));
+
+        public PlayerModelOptions modelOptionsOrDefault() {
+            return this.modelOptions().orElse(PlayerModelOptions.DEFAULT);
+        }
     }
 }
