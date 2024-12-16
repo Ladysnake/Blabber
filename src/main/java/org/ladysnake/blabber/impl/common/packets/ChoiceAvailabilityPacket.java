cec39f3bf4ede33b54bec7481431c5ae3afb21e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-12-16 20:00:45 +0900

    port

--- src/main/java/org/ladysnake/blabber/impl/common/packets/ChoiceAvailabilityPacket.java
+++ src/main/java/org/ladysnake/blabber/impl/common/packets/ChoiceAvailabilityPacket.java
@@ -19,46 +19,49 @@ package org.ladysnake.blabber.impl.common.packets;
 
 import it.unimi.dsi.fastutil.ints.Int2BooleanMap;
 import it.unimi.dsi.fastutil.ints.Int2BooleanOpenHashMap;
-import net.fabricmc.fabric.api.networking.v1.FabricPacket;
-import net.fabricmc.fabric.api.networking.v1.PacketType;
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.network.PacketDistributor;
 import net.minecraft.network.PacketByteBuf;
 import org.ladysnake.blabber.Blabber;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * Represents a list of dialogue choices which availability has changed
  */
-public record ChoiceAvailabilityPacket(Map<String, Int2BooleanMap> updatedChoices) implements FabricPacket {
-    public static final PacketType<ChoiceAvailabilityPacket> TYPE = PacketType.create(Blabber.id("choice_availability"), ChoiceAvailabilityPacket::new);
+public class ChoiceAvailabilityPacket {
+    private final Map<String, Int2BooleanMap> updatedChoices;
 
     public ChoiceAvailabilityPacket() {
-        this(new HashMap<>());
+        this.updatedChoices = new HashMap<>();
     }
 
     public ChoiceAvailabilityPacket(PacketByteBuf buf) {
-        this(buf.readMap(
+        this.updatedChoices = buf.readMap(
                 PacketByteBuf::readString,
                 b -> b.readMap(Int2BooleanOpenHashMap::new, PacketByteBuf::readVarInt, PacketByteBuf::readBoolean)
-        ));
+        );
     }
 
-    @Override
     public void write(PacketByteBuf buf) {
         buf.writeMap(
-                this.updatedChoices(),
+                this.updatedChoices,
                 PacketByteBuf::writeString,
                 (b, updatedChoices) -> b.writeMap(updatedChoices, PacketByteBuf::writeVarInt, PacketByteBuf::writeBoolean)
         );
     }
 
-    @Override
-    public PacketType<?> getType() {
-        return TYPE;
+    public void handle(Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> {
+            // Handle the packet
+        });
+        context.setPacketHandled(true);
     }
 
     public void markUpdated(String stateKey, int choiceIndex, boolean newValue) {
-        this.updatedChoices().computeIfAbsent(stateKey, s -> new Int2BooleanOpenHashMap()).put(choiceIndex, newValue);
+        this.updatedChoices.computeIfAbsent(stateKey, s -> new Int2BooleanOpenHashMap()).put(choiceIndex, newValue);
     }
 }
