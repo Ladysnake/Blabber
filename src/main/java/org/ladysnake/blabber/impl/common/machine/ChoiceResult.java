d224e8d75fc255e8135fd0aed68de7e7c2a903b1
Pyrofab <pyrofab@ladysnake.org>
2025-04-20 20:05:21 +0200

    Update license headers

--- src/main/java/org/ladysnake/blabber/impl/common/machine/ChoiceResult.java
+++ src/main/java/org/ladysnake/blabber/impl/common/machine/ChoiceResult.java
@@ -1,6 +1,6 @@
 /*
  * Blabber
- * Copyright (C) 2022-2024 Ladysnake
+ * Copyright (C) 2022-2025 Ladysnake
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
